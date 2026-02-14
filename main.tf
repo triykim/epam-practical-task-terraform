d7fa1ebcdda9962e245f5b6ad9b387ecfab861e5
triykim <44566650@gmail.com>
2026-02-14 11:51:57 +0500

    Added local vars, ec2 instances

--- main.tf
+++ main.tf
@@ -1,21 +1,62 @@
 provider "aws" {
-  region  = "us-east-1"
+  region  = local.region
   profile = "default"
 }
 
-module "vpc" {
-  source = "terraform-aws-modules/vpc/aws"
+locals {
+  region = "us-east-1"
 
-  name = "custom-vpc"
-  cidr = "10.0.0.0/16"
-
-  azs             = ["us-east-1a", "us-east-1b"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+  vpc_cidr = "10.0.0.0/16"
+  azs      = slice(data.aws_availability_zones.available.names, 0, 2)
 
   tags = {
     Terraform = "true"
     Environment = "dev"
     Project = "epam"
   }
+}
+
+data "aws_availability_zones" "available" {}
+
+data "aws_ami" "al_latest" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["al2023-ami-2023*"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+}
+
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "custom-vpc"
+  cidr = local.vpc_cidr
+
+  azs             = local.azs
+  private_subnets = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k)]
+  public_subnets = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k + 100)]
+
+  tags = local.tags
+}
+
+resource "aws_instance" "app_server" {
+  count = 2
+  ami           = data.aws_ami.al_latest.id
+  instance_type = "t3.micro"
+  vpc_security_group_ids = [module.vpc.default_security_group_id]
+  subnet_id = module.vpc.private_subnets[count.index]
+
+  tags = merge(
+    local.tags,
+    {
+      Name = "app_server${count.index + 1}"
+    }
+  )
 }
